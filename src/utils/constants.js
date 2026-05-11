6b2fe970c74f74932b7858b463c60b0a7378645a
Pavan V <pavanvpavi777@gmail.com>
2026-05-11 14:57:10 +0530

    Fix API URL for production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://localhost:8080/api/v1";
+// export const API_BASE_URL = "http://localhost:8080/api/v1";
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const ORDER_STATUS_COLORS = {
   DRAFT: "badge-gray",
